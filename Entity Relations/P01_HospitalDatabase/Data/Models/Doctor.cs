61b054241e3875f207e283a3bfe1fe852ba57c79
kstefanov4 <103167025+kstefanov4@users.noreply.github.com>
2023-03-10 02:14:19 +0200

    C#

---
+++ Entity Relations/P01_HospitalDatabase/Data/Models/Doctor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P01_HospitalDatabase.Data.Models
+{
+    public class Doctor
+    {
+        public Doctor()
+        {
+            this.Visitations = new HashSet<Visitation>();
+        }
+        [Key]
+        public int DoctorId { get; set; }
+        [MaxLength(100)]
+        [Unicode]
+        public string Name { get; set; } = null!;
+        [MaxLength(100)]
+        [Unicode]
+        public string Specialty { get; set; } = null!;
+        public virtual ICollection<Visitation> Visitations { get; set; } = null!;
+    }
+}
